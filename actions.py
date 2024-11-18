28b794949163ba68505bfb312ef270484739c822
MaxencePorgeon <maxence.porgeon@edu.esiee.fr>
2024-11-18 17:33:40 +0000

    haha

--- actions.py
+++ actions.py
@@ -4,8 +4,8 @@
 # Each function takes 3 parameters:
 # - game: the game object
 # - list_of_words: the list of words in the command
-# - number_of_parameters: the number of parameters expected by the command
-# The functions return True if the command was executed successfully, False otherwise.
+# - number_of_parameters: the number of parameters expected by the command hhh
+# The functions return True if the command was executed successfully, False otherwise. 
 # The functions print an error message if the number of parameters is incorrect.
 # The error message is different depending on the number of parameters expected by the command.
 
