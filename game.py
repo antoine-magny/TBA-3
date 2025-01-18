41b516b7547593b6eb2ed9011df71b01e41024e3
antoine-magny <antoine.magny@edu.esiee.fr>
2025-01-18 16:29:28 +0000

    sdf

--- game.py
+++ game.py
@@ -11,91 +11,77 @@ from character import Character
 import random
 from config import DEBUG
 
+class Game :
+    """
+    Classe principale du jeu d'aventure textuelle.
 
-class Game:
+    Attributs:
+        finished (bool): Indique si le jeu est terminé.
+        rooms (list[Room]): Liste des salles du jeu.
+        commands (dict): Commandes disponibles dans le jeu.
+        player (Player): Joueur principal.
 
+    Méthodes:
+        setup(): Configure les salles, commandes et objets du jeu.
+        play(): Lance la boucle principale du jeu.
+        process_command(command_string): Traite une commande saisie par le joueur.
+        print_welcome(): Affiche le message de bienvenue.
+    """
     # Constructor
     def __init__(self):
         self.finished = False
         self.rooms = []
         self.commands = {}
         self.player = None
-    
-    
-    
     # Setup the game
     def setup(self):
         # Setup commands
-        
         help = Command("help", " : afficher cette aide", Actions.help, 0)
         self.commands["help"] = help
-        
         quit = Command("quit", " : quitter le jeu", Actions.quit, 0)
         self.commands["quit"] = quit
-        self.commands["exit"] = quit    
-        
+        self.commands["exit"] = quit
         go = Command("go", " <direction> : se déplacer dans une direction cardinale (N, E, S, O, U, D)", Actions.go, 1)
         self.commands["go"] = go
-        
         history = Command("history", " <direction> : se déplacer dans une direction cardinale (N, E, S, O, U, D)", Actions.history, 1)
         self.commands["history"] = history
-        
         back = Command("back", " : revenir en arrièrre", Actions.back, 0)
         self.commands["back"] = back
-        
         check = Command("check", " : vérifier les objets dans votre inventaire", Actions.check, 0)
         self.commands["check"] = check
-        
         look = Command("look", " : observer les objets présents dans sur l'île", Actions.look, 0)
         self.commands["look"] = look
-        
         take = Command("take", " 'objet' : prendre un objet sur l'île", Actions.take, 1)
         self.commands["take"] = take
-        
         drop = Command("drop", " 'objet' : poser un objet sur l'île", Actions.drop, 1)
         self.commands["drop"] = drop
-        
         talk = Command("talk", " <nom du PNJ> : discuter avec un personnage non joueur", Actions.talk, 1)
         self.commands["talk"] = talk
-        
-        
-        # Nouveaux lieux : 
-        
+
+        # Nouveaux lieux :
         # Attention : format des descriptions des îles : "Vous êtes" + "description de l'île"
-        
         Arlong_Park = Room("Arlong_Park", "dans la base redoutable d'Arlong et de ses hommes-poissons. Une navigatrice y est retenue prisonnière.")
         self.rooms.append(Arlong_Park)
-
         Fuschia_Village = Room("Fuschia_Village", "dans le paisible village natal de Luffy, situé en bord de mer. Attention aux monstres marins qui rôdent autour !")
         self.rooms.append(Fuschia_Village)
-
         Shells_Town = Room("Shells_Town", "dans une ville où la Marine est omniprésente, dominée par une immense base militaire. Un certain Capitaine Morgan y fait régner la terreur. Il retient prisonnier un sabreur et un tireur d'élite.")
         self.rooms.append(Shells_Town)
-
         Baratie = Room("Baratie", "dans un restaurant flottant connu pour sa cuisine exceptionnelle et ses batailles épiques. Un certain Sanji y est chef cuisinier.")
         self.rooms.append(Baratie)
-
         Grand_Line = Room("Grand Line", "au milieu des mers se trouve un passage à sens unique vers la route des pirates !")
         self.rooms.append(Grand_Line)
-
         Royaume_de_Drum = Room("Royaume de Drum", "dans une île où le froid règne, on y trouve un certain médecin appelé Chopper.")
         self.rooms.append(Royaume_de_Drum)
-
         Pays_de_Wa = Room("Pays de Wa", "un pays où les samouraïs vivent en paix.")
         self.rooms.append(Pays_de_Wa)
-
         Dresserosa = Room("Dresserosa", "dans une ville où règne un tyran qui transforme ses malfaiteurs en jouets pour enfants.")
         self.rooms.append(Dresserosa)
-
         Little_Garden = Room("Little Garden", "une île où deux géants de 100 ans se battent pour la gloire.")
         self.rooms.append(Little_Garden)
-
         Alabasta = Room("Alabasta", "un royaume désertique où règne une lutte contre un crocodile des sables.")
         self.rooms.append(Alabasta)
-
         Skypiea = Room("Skypiea", "une île céleste, flottant dans un océan de nuages.")
         self.rooms.append(Skypiea)
-
         Laugh_Tale = Room("Laugh Tale", "une île légendaire : la dernière île de Grand Line. Vous avez trouvé le One Piece ! Félicitations !")
         self.rooms.append(Laugh_Tale)
 
@@ -103,7 +89,7 @@ class Game:
         Fuschia_Village.exits = {"N": Arlong_Park, "E": Shells_Town, "S": Grand_Line, "O": Baratie, "U": None, "D": None}
         Arlong_Park.exits = {"N": None, "E": None, "S": Fuschia_Village, "O": None, "U": None, "D": None}
         Shells_Town.exits = {"N": None, "E": None, "S": None, "O": Fuschia_Village, "U": None, "D": None}
-        Baratie.exits = {"N": None, "E": Fuschia_Village, "S": None, "O": None, "U": None, "D": None}    
+        Baratie.exits = {"N": None, "E": Fuschia_Village, "S": None, "O": None, "U": None, "D": None}
         Grand_Line.exits = {"N": None, "E": Royaume_de_Drum, "S": Alabasta, "O": Little_Garden, "U": None, "D": None}
         Royaume_de_Drum.exits = {"N": None, "E": None, "S": Pays_de_Wa, "O": Grand_Line, "U": None, "D": None}
         Pays_de_Wa.exits = {"N": Royaume_de_Drum, "E": None, "S": Laugh_Tale, "O": Alabasta, "U": None, "D": None}
@@ -112,22 +98,20 @@ class Game:
         Little_Garden.exits = {"N": None, "E": Grand_Line, "S": Dresserosa, "O": None, "U": Skypiea, "D": None}
         Skypiea.exits = {"N": None, "E": None, "S": None, "O": None, "U": None, "D": Little_Garden}
         Laugh_Tale.exits = {"N": Pays_de_Wa, "E": None, "S": None, "O": None, "U": None, "D": None}
-        
-        
+
         # Ajout des objets sur les îles
         gomu_gomu_no_mi = {"description": "Fruit du démon qui donne des pouvoirs élastiques", "weight": 0.5}
         Shimotsuki_Kozaburo_Katanas = {"description": "2 des 3 katanas de Zoro", "weight": 4.0}
-        wado_ichimonji = {"description": "L'épée donnée à titre posthume par Kuina à zoro ", "weight": 2.0}        
+        wado_ichimonji = {"description": "L'épée donnée à titre posthume par Kuina à zoro ", "weight": 2.0}
         shusui = {"description": "L'épée légendaire du samouraï Ryuma", "weight": 2.0}
-        lance_pierre = {"description": "L'arme de prédilection d'Usopp", "weight": 1.0}        
-        Rumble_Ball = {"description": "Une Drogue rendant Chopper très puissant", "weight": 0.5} 
+        lance_pierre = {"description": "L'arme de prédilection d'Usopp", "weight": 1.0}
+        Rumble_Ball = {"description": "Une Drogue rendant Chopper très puissant", "weight": 0.5}
         Hana_Hana_no_Mi = {"description": "Fruit du démon des éclosions", "weight": 0.5}
         Ito_Ito_No_Mi = {"description": "Fruit du démon des fils", "weight": 0.5}
         Hache_du_Cogneur = {"description": "Une hache pour Dorry, un des géants de Little Garden", "weight": 100.0}
         Epee_Brogy = {"description": "Une épée pour Brogy, un des géants les géants de Little Garden", "weight": 75.0}
         Uo_Uo_no_Mi = {"description": "Fruit du dragon oriental", "weight": 4.0}
         One_piece = {"description": "Le trésor le plus Grand de tout les temps"}
-
 
         # Ajout des objets sur les îles
         # lieu.inventory[clé] = valeur (la valeur étant le dictionnaire définit précédement)
@@ -144,7 +128,7 @@ class Game:
         Pays_de_Wa.inventory["Uo Uo no Mi"]= Uo_Uo_no_Mi
         Laugh_Tale.inventory["One Piece"]= One_piece
 
-        # Personnages : 
+        # Personnages :
         # Ajout des PNJ dans les pièces
         makino = Character("Makino", None, "La serveuse du bar de Fuschia Village", ["Tu veux une bière, mon ptit ?", "Bois un jus Luffy !"])
         arlong = Character("Arlong", None, "Chef des hommes-poissons et maître d'Arlong Park", ["Rendez-vous humains !", "Nami sera à moi pour toujours."])
@@ -156,9 +140,7 @@ class Game:
         Brogy =Character("Brogy", None,"Un géant se battant pour la gloire" ,["Un siècle... ce fut vraiment un long combat!"])
         Doflamigo = Character("Doflamigo", None,"Un tiran animé par le pouvoir" ,["Les faibles ne pourront pas s'échapper, les forts feront la loi, place à une nouvelle ère !"])
         Momo = Character("Momo", None,"Un jeune souverain du Pays de Wa et fils du héros de ce pays" ,["Je te le jures Luffy, un jour je battrais battre Kaido !"])
-        
-        
-        
+
         Fuschia_Village.characters = {"Makino": makino}
         Arlong_Park.characters = {"Arlong": arlong}
         Shells_Town.characters = {"Zoro": Zoro}
@@ -168,12 +150,11 @@ class Game:
         Little_Garden.characters = {"Brogy": Brogy}
         Dresserosa.characters = {"Doflamigo": Doflamigo}
         Pays_de_Wa.characters = {"Momo": Momo}
-        
+
         # Setup player and starting room
         self.player = Player(input("\nEntrez votre nom: "))
         self.player.current_room = Fuschia_Village
-    
-    
+
     def move_characters(self):
         """Déplace un seul PNJ aléatoire vers une sortie aléatoire par tour."""
         # Créer une liste de tous les PNJs dans toutes les pièces
@@ -181,12 +162,11 @@ class Game:
         for room in self.rooms:
             for name, character in room.characters.items():
                 all_characters.append((name, character, room))
-        
         if not all_characters:
             return  # Aucun PNJ à déplacer, on sort de la fonction
-        
+
         name, character, current_room = random.choice(all_characters)
-        
+
         exits = []
         for exit in current_room.exits.values():
             if isinstance(exit, Room):
@@ -194,7 +174,7 @@ class Game:
 
         if not exits:
             return  # Pas de sorties valides pour déplacer le PNJ
-        
+
         # Déplacer le PNJ vers une pièce aléatoire
         new_room = random.choice(exits)
         new_room.characters[name] = character
@@ -203,21 +183,20 @@ class Game:
         # Afficher un message pour signaler le déplacement
         print(f"\n{character.name} s'est déplacé vers {new_room.name}.")
 
-
     # Play the game
     def play(self):
         self.setup()
         self.print_welcome()
-        
+
         # Loop until the game is finished
-        while self.finished != True:
+        while self.finished is not True:
             self.process_command(input("> "))
             self.move_characters()
         return None
 
     # Process the command entered by the player
     def process_command(self, command_string) -> None:
-        
+
         #Affiche rien lorsque le joueur tape "entrer"
         if command_string=='':
             return None
@@ -244,7 +223,6 @@ class Game:
 def main():
     # Create a game object and play the game
     Game().play()
-    
 
 if __name__ == "__main__":
     main()
